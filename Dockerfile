cecc7da8874ecdb2d0a553d4ae495dfffd9f0e08
mrog <4752942+mrog@users.noreply.github.com>
2025-08-15 23:54:55 +0000

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+# syntax=docker/dockerfile:1
+FROM modem7/docker-rickroll
+ARG UID=101
+
+USER root
+COPY --link ./index.html /usr/share/nginx/html/guest/s/default/
+RUN chown $UID:0 /usr/share/nginx/html/index.html
+USER $UID
+
---
+++ upgrade.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+container_name=unifi-hotspot-rickroll
+
+echo
+echo ====================================
+echo Upgrading $container_name
+date
+echo
+
+# Download and build the latest version.
+docker-compose build --no-cache
+
+# Is the container running?
+IS_RUNNING=$(docker container inspect -f '{{.State.Running}}' "$container_name" || echo "false")
+
+if [ "$IS_RUNNING" = "true" ]; then
+    echo "$container_name is running, so it will now be restarted."
+    # Note that `docker-compose restart` won't have the same effect.
+    docker-compose stop
+    docker container rm "$container_name" || true
+    docker-compose up -d
+else
+    echo "$container_name isn't running, so it won't be started automatically.  Please start it manually."
+fi
